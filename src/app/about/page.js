cab33618434a53db8bf58f7663ee909a36b69866
pawantechofficial <pawan81222@gmail.com>
2024-02-06 11:45:32 +0530

    about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,25 +1,7 @@
-import ImageSlider from "@/components/ImageSlider/ImageSlider";
 import React from "react";
 
 const page = () => {
-  return (
-    <section className="bg-white w-full justify-center mx-auto">
-      <div className="h-[30%] md:h-[50%] xl:h-[90%] w-auto mx-auto overflow-hidden object-fill p-2 pt-20">
-        <div className="">
-          <ImageSlider />
-        </div>
-      </div>
-      <div className="w-full flex text-center flex-col justify-center mx-auto">
-        <h1 className="text-black text-2xl">Hello</h1>
-        <p className="text-left">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. At iure modi
-          quas veritatis, incidunt dolorem magnam, vero reiciendis doloribus
-          distinctio quia repellat praesentium nihil rerum nostrum sit ad cumque
-          ea?
-        </p>
-      </div>
-    </section>
-  );
+  return <div>page</div>;
 };
 
 export default page;
